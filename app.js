2b0f1a10f7b45fef80adecffae9205f5bcae56b7
Madan Tamang <click2madan@gmail.com>
2017-03-23 16:32:18 +0545

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ var app = express();
  * Connect to MongoDB.
  */
 
-mongoose.connect(process.env.MONGODB || process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
 mongoose.connection.on('error', function() {
   console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
   process.exit(1);
@@ -77,7 +77,7 @@ app.use(session({
   saveUninitialized: true,
   secret: process.env.SESSION_SECRET,
   store: new MongoStore({
-    url: process.env.MONGODB || process.env.MONGOLAB_URI,
+    url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
     autoReconnect: true
   })
 }));
